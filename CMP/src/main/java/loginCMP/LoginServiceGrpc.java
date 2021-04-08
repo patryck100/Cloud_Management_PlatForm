f59ca5c6a93056e21eaa01f28b44fced4e83e1b0
Patryck Brenner <patryck_brenner@outlook.com>
2021-04-08 21:14:32 +0100

    Login and Logout Service and proto file

---
+++ CMP/src/main/java/loginCMP/LoginServiceGrpc.java
@@ -0,0 +1,382 @@
+package loginCMP;
+
+import static io.grpc.MethodDescriptor.generateFullMethodName;
+import static io.grpc.stub.ClientCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncUnaryCall;
+import static io.grpc.stub.ClientCalls.blockingServerStreamingCall;
+import static io.grpc.stub.ClientCalls.blockingUnaryCall;
+import static io.grpc.stub.ClientCalls.futureUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
+
+/**
+ * <pre>
+ * The services goes here.
+ * </pre>
+ */
+@javax.annotation.Generated(
+    value = "by gRPC proto compiler (version 1.15.0)",
+    comments = "Source: loginCMP.proto")
+public final class LoginServiceGrpc {
+
+  private LoginServiceGrpc() {}
+
+  public static final String SERVICE_NAME = "CMP.LoginService";
+
+  // Static method descriptors that strictly reflect the proto.
+  private static volatile io.grpc.MethodDescriptor<loginCMP.LoginRequest,
+      loginCMP.LoginResponse> getLoginMethod;
+
+  @io.grpc.stub.annotations.RpcMethod(
+      fullMethodName = SERVICE_NAME + '/' + "login",
+      requestType = loginCMP.LoginRequest.class,
+      responseType = loginCMP.LoginResponse.class,
+      methodType = io.grpc.MethodDescriptor.MethodType.UNARY)
+  public static io.grpc.MethodDescriptor<loginCMP.LoginRequest,
+      loginCMP.LoginResponse> getLoginMethod() {
+    io.grpc.MethodDescriptor<loginCMP.LoginRequest, loginCMP.LoginResponse> getLoginMethod;
+    if ((getLoginMethod = LoginServiceGrpc.getLoginMethod) == null) {
+      synchronized (LoginServiceGrpc.class) {
+        if ((getLoginMethod = LoginServiceGrpc.getLoginMethod) == null) {
+          LoginServiceGrpc.getLoginMethod = getLoginMethod = 
+              io.grpc.MethodDescriptor.<loginCMP.LoginRequest, loginCMP.LoginResponse>newBuilder()
+              .setType(io.grpc.MethodDescriptor.MethodType.UNARY)
+              .setFullMethodName(generateFullMethodName(
+                  "CMP.LoginService", "login"))
+              .setSampledToLocalTracing(true)
+              .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  loginCMP.LoginRequest.getDefaultInstance()))
+              .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  loginCMP.LoginResponse.getDefaultInstance()))
+                  .setSchemaDescriptor(new LoginServiceMethodDescriptorSupplier("login"))
+                  .build();
+          }
+        }
+     }
+     return getLoginMethod;
+  }
+
+  private static volatile io.grpc.MethodDescriptor<loginCMP.LogoutRequest,
+      loginCMP.LogoutResponse> getLogoutMethod;
+
+  @io.grpc.stub.annotations.RpcMethod(
+      fullMethodName = SERVICE_NAME + '/' + "logout",
+      requestType = loginCMP.LogoutRequest.class,
+      responseType = loginCMP.LogoutResponse.class,
+      methodType = io.grpc.MethodDescriptor.MethodType.UNARY)
+  public static io.grpc.MethodDescriptor<loginCMP.LogoutRequest,
+      loginCMP.LogoutResponse> getLogoutMethod() {
+    io.grpc.MethodDescriptor<loginCMP.LogoutRequest, loginCMP.LogoutResponse> getLogoutMethod;
+    if ((getLogoutMethod = LoginServiceGrpc.getLogoutMethod) == null) {
+      synchronized (LoginServiceGrpc.class) {
+        if ((getLogoutMethod = LoginServiceGrpc.getLogoutMethod) == null) {
+          LoginServiceGrpc.getLogoutMethod = getLogoutMethod = 
+              io.grpc.MethodDescriptor.<loginCMP.LogoutRequest, loginCMP.LogoutResponse>newBuilder()
+              .setType(io.grpc.MethodDescriptor.MethodType.UNARY)
+              .setFullMethodName(generateFullMethodName(
+                  "CMP.LoginService", "logout"))
+              .setSampledToLocalTracing(true)
+              .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  loginCMP.LogoutRequest.getDefaultInstance()))
+              .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  loginCMP.LogoutResponse.getDefaultInstance()))
+                  .setSchemaDescriptor(new LoginServiceMethodDescriptorSupplier("logout"))
+                  .build();
+          }
+        }
+     }
+     return getLogoutMethod;
+  }
+
+  /**
+   * Creates a new async stub that supports all call types for the service
+   */
+  public static LoginServiceStub newStub(io.grpc.Channel channel) {
+    return new LoginServiceStub(channel);
+  }
+
+  /**
+   * Creates a new blocking-style stub that supports unary and streaming output calls on the service
+   */
+  public static LoginServiceBlockingStub newBlockingStub(
+      io.grpc.Channel channel) {
+    return new LoginServiceBlockingStub(channel);
+  }
+
+  /**
+   * Creates a new ListenableFuture-style stub that supports unary calls on the service
+   */
+  public static LoginServiceFutureStub newFutureStub(
+      io.grpc.Channel channel) {
+    return new LoginServiceFutureStub(channel);
+  }
+
+  /**
+   * <pre>
+   * The services goes here.
+   * </pre>
+   */
+  public static abstract class LoginServiceImplBase implements io.grpc.BindableService {
+
+    /**
+     * <pre>
+     *unary(unique request and response) method will be implemented in the server
+     * </pre>
+     */
+    public void login(loginCMP.LoginRequest request,
+        io.grpc.stub.StreamObserver<loginCMP.LoginResponse> responseObserver) {
+      asyncUnimplementedUnaryCall(getLoginMethod(), responseObserver);
+    }
+
+    /**
+     */
+    public void logout(loginCMP.LogoutRequest request,
+        io.grpc.stub.StreamObserver<loginCMP.LogoutResponse> responseObserver) {
+      asyncUnimplementedUnaryCall(getLogoutMethod(), responseObserver);
+    }
+
+    @java.lang.Override public final io.grpc.ServerServiceDefinition bindService() {
+      return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
+          .addMethod(
+            getLoginMethod(),
+            asyncUnaryCall(
+              new MethodHandlers<
+                loginCMP.LoginRequest,
+                loginCMP.LoginResponse>(
+                  this, METHODID_LOGIN)))
+          .addMethod(
+            getLogoutMethod(),
+            asyncUnaryCall(
+              new MethodHandlers<
+                loginCMP.LogoutRequest,
+                loginCMP.LogoutResponse>(
+                  this, METHODID_LOGOUT)))
+          .build();
+    }
+  }
+
+  /**
+   * <pre>
+   * The services goes here.
+   * </pre>
+   */
+  public static final class LoginServiceStub extends io.grpc.stub.AbstractStub<LoginServiceStub> {
+    private LoginServiceStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private LoginServiceStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected LoginServiceStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new LoginServiceStub(channel, callOptions);
+    }
+
+    /**
+     * <pre>
+     *unary(unique request and response) method will be implemented in the server
+     * </pre>
+     */
+    public void login(loginCMP.LoginRequest request,
+        io.grpc.stub.StreamObserver<loginCMP.LoginResponse> responseObserver) {
+      asyncUnaryCall(
+          getChannel().newCall(getLoginMethod(), getCallOptions()), request, responseObserver);
+    }
+
+    /**
+     */
+    public void logout(loginCMP.LogoutRequest request,
+        io.grpc.stub.StreamObserver<loginCMP.LogoutResponse> responseObserver) {
+      asyncUnaryCall(
+          getChannel().newCall(getLogoutMethod(), getCallOptions()), request, responseObserver);
+    }
+  }
+
+  /**
+   * <pre>
+   * The services goes here.
+   * </pre>
+   */
+  public static final class LoginServiceBlockingStub extends io.grpc.stub.AbstractStub<LoginServiceBlockingStub> {
+    private LoginServiceBlockingStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private LoginServiceBlockingStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected LoginServiceBlockingStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new LoginServiceBlockingStub(channel, callOptions);
+    }
+
+    /**
+     * <pre>
+     *unary(unique request and response) method will be implemented in the server
+     * </pre>
+     */
+    public loginCMP.LoginResponse login(loginCMP.LoginRequest request) {
+      return blockingUnaryCall(
+          getChannel(), getLoginMethod(), getCallOptions(), request);
+    }
+
+    /**
+     */
+    public loginCMP.LogoutResponse logout(loginCMP.LogoutRequest request) {
+      return blockingUnaryCall(
+          getChannel(), getLogoutMethod(), getCallOptions(), request);
+    }
+  }
+
+  /**
+   * <pre>
+   * The services goes here.
+   * </pre>
+   */
+  public static final class LoginServiceFutureStub extends io.grpc.stub.AbstractStub<LoginServiceFutureStub> {
+    private LoginServiceFutureStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private LoginServiceFutureStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected LoginServiceFutureStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new LoginServiceFutureStub(channel, callOptions);
+    }
+
+    /**
+     * <pre>
+     *unary(unique request and response) method will be implemented in the server
+     * </pre>
+     */
+    public com.google.common.util.concurrent.ListenableFuture<loginCMP.LoginResponse> login(
+        loginCMP.LoginRequest request) {
+      return futureUnaryCall(
+          getChannel().newCall(getLoginMethod(), getCallOptions()), request);
+    }
+
+    /**
+     */
+    public com.google.common.util.concurrent.ListenableFuture<loginCMP.LogoutResponse> logout(
+        loginCMP.LogoutRequest request) {
+      return futureUnaryCall(
+          getChannel().newCall(getLogoutMethod(), getCallOptions()), request);
+    }
+  }
+
+  private static final int METHODID_LOGIN = 0;
+  private static final int METHODID_LOGOUT = 1;
+
+  private static final class MethodHandlers<Req, Resp> implements
+      io.grpc.stub.ServerCalls.UnaryMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ServerStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ClientStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.BidiStreamingMethod<Req, Resp> {
+    private final LoginServiceImplBase serviceImpl;
+    private final int methodId;
+
+    MethodHandlers(LoginServiceImplBase serviceImpl, int methodId) {
+      this.serviceImpl = serviceImpl;
+      this.methodId = methodId;
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public void invoke(Req request, io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        case METHODID_LOGIN:
+          serviceImpl.login((loginCMP.LoginRequest) request,
+              (io.grpc.stub.StreamObserver<loginCMP.LoginResponse>) responseObserver);
+          break;
+        case METHODID_LOGOUT:
+          serviceImpl.logout((loginCMP.LogoutRequest) request,
+              (io.grpc.stub.StreamObserver<loginCMP.LogoutResponse>) responseObserver);
+          break;
+        default:
+          throw new AssertionError();
+      }
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public io.grpc.stub.StreamObserver<Req> invoke(
+        io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        default:
+          throw new AssertionError();
+      }
+    }
+  }
+
+  private static abstract class LoginServiceBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoFileDescriptorSupplier, io.grpc.protobuf.ProtoServiceDescriptorSupplier {
+    LoginServiceBaseDescriptorSupplier() {}
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.FileDescriptor getFileDescriptor() {
+      return loginCMP.LoginServiceImpl.getDescriptor();
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.ServiceDescriptor getServiceDescriptor() {
+      return getFileDescriptor().findServiceByName("LoginService");
+    }
+  }
+
+  private static final class LoginServiceFileDescriptorSupplier
+      extends LoginServiceBaseDescriptorSupplier {
+    LoginServiceFileDescriptorSupplier() {}
+  }
+
+  private static final class LoginServiceMethodDescriptorSupplier
+      extends LoginServiceBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoMethodDescriptorSupplier {
+    private final String methodName;
+
+    LoginServiceMethodDescriptorSupplier(String methodName) {
+      this.methodName = methodName;
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.MethodDescriptor getMethodDescriptor() {
+      return getServiceDescriptor().findMethodByName(methodName);
+    }
+  }
+
+  private static volatile io.grpc.ServiceDescriptor serviceDescriptor;
+
+  public static io.grpc.ServiceDescriptor getServiceDescriptor() {
+    io.grpc.ServiceDescriptor result = serviceDescriptor;
+    if (result == null) {
+      synchronized (LoginServiceGrpc.class) {
+        result = serviceDescriptor;
+        if (result == null) {
+          serviceDescriptor = result = io.grpc.ServiceDescriptor.newBuilder(SERVICE_NAME)
+              .setSchemaDescriptor(new LoginServiceFileDescriptorSupplier())
+              .addMethod(getLoginMethod())
+              .addMethod(getLogoutMethod())
+              .build();
+        }
+      }
+    }
+    return result;
+  }
+}
